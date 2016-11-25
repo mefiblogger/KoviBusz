45b514745d0ec17bdbc38e37d2b7af8a7eacca13
Fási Gábor <fasi.gabor@ingatlan.com>
2016-11-25 15:14:18 +0100

    108E kezelese arkon tickerben

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get("/arkon/ticker", function (req, res) {
         responseCallback = function (error, data) {
             responses.push(data);
 
-            if (responses.length == 5) { //megjott minden info
+            if (responses.length == 6) { //megjott minden info
                 var output = "";
 
                 for (var arrivalId in responses) {
@@ -104,6 +104,7 @@ app.get("/arkon/ticker", function (req, res) {
 
     request.getNextArrival("F00002", "BKK_1780", responseCallback); // 178
     request.getNextArrival("F00004", "BKK_0085", responseCallback); // 8E
+    request.getNextArrival("F00004", "BKK_1085", responseCallback); // 108E
     request.getNextArrival("F00004", "BKK_1100", responseCallback); // 110
     request.getNextArrival("F00004", "BKK_1120", responseCallback); // 112
     request.getNextArrival("F00004", "BKK_9080", responseCallback); // 908
